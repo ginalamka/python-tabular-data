60682f8dea929981ef2532198183bb1e74f28dd4
Gina F. Lamka <gfl0003@auburn.edu>
2021-04-14 14:44:11 -0400

    created script.py
    This will be used to create a scipt that will plot two variables
    from iris data.

---
+++ script.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+"""
+A script to plot two variables stored in columns of a table data file.
+"""
+
+import os
+import sys
+import argparse
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+
+
+
+
+if __name__ == '__main__':
+    main_cli()
